d81e759dac8ceb518780ea7df47bcf067456fde7
Samuel <samuel.a.lopes@gmail.com>
2025-05-16 20:56:06 -0300

    ~ update script

--- tray.js
+++ tray.js
@@ -1,31 +1,19 @@
 (function () {
-    console.log("funcionou");
+    var kdocument = $('#customer-cpf').val();
+    var msg = 'nao deu tempo';
+    if (!kdocument) {
+        msg = 'achou CPF'
+    }else{
 
-    // === Inserção de div com JavaScript puro ===
-    const divPura = document.createElement("div");
-    divPura.textContent = "Div adicionada com JavaScript puro";
-    divPura.style.backgroundColor = "#cce5ff";
-    divPura.style.padding = "10px";
-    divPura.style.margin = "10px 0";
-    divPura.style.display = "none";
-    divPura.style.border = "1px solid #007bff";
-
-    // Insere no body ou outro lugar se quiser
-    document.body.appendChild(divPura);
-
-    // === Inserção de div com jQuery (se disponível) ===
-    if (typeof window.jQuery !== "undefined") {
-        $("<div>")
-            .text("Div adicionada com jQuery")
-            .css({
-                backgroundColor: "#d4edda",
-                padding: "10px",
-                display: "none",
-                margin: "10px 0",
-                border: "1px solid #28a745"
-            })
-            .appendTo("body");
-    } else {
-        console.warn("jQuery não está disponível.");
     }
+    $("<div>")
+        .text(msg)
+        .css({
+            backgroundColor: "#d4edda",
+            padding: "10px",
+            display: "none",
+            margin: "10px 0",
+            border: "1px solid #28a745"
+        })
+        .appendTo("body");
 })();
