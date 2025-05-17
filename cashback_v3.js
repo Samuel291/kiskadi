8fc492546e63f75479524a98f92b4bc88b1af1bd
Samuel <samuel.a.lopes@gmail.com>
2025-05-17 01:00:55 -0300

    ~ update scripts

--- cashback_v3.js
+++ cashback_v3.js
@@ -1,21 +1,25 @@
 (function () {
-    const target = document.querySelector('#customer-cpf');
-    if (target) {
-        const observer = new MutationObserver((mutationsList) => {
-            mutationsList.forEach((mutation) => {
-                // if (mutation.type === 'attributes' && mutation.attributeName === 'value') {
-                    console.log('Valor alterado:', target.value);
-                // }
-            });
-        });
-
-        observer.observe(target, {
-            attributes: true,
-            // attributeFilter: ['value'] // só observa o atributo value
-        });
-
-        console.log('Observando mudanças no atributo value de #customer-cpf');
-    } else {
-        console.warn('Elemento #customer-cpf não encontrado.');
-    }
+    console.log($('#customer-cpf'))
+    console.log($('#customer-cpf').val())
+    console.log($('#customer-cpf').value)
+    console.log($('#customer-cpf').text)
+    // const target = document.querySelector('#customer-cpf');
+    // if (target) {
+    //     const observer = new MutationObserver((mutationsList) => {
+    //         mutationsList.forEach((mutation) => {
+    //             // if (mutation.type === 'attributes' && mutation.attributeName === 'value') {
+    //                 console.log('Valor alterado:', target.value);
+    //             // }
+    //         });
+    //     });
+    //
+    //     observer.observe(target, {
+    //         attributes: true,
+    //         // attributeFilter: ['value'] // só observa o atributo value
+    //     });
+    //
+    //     console.log('Observando mudanças no atributo value de #customer-cpf');
+    // } else {
+    //     console.warn('Elemento #customer-cpf não encontrado.');
+    // }
 })();
