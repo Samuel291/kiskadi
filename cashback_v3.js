bb18c87b6faabf7c836cd0e69e82dea66b2e53c3
Samuel <samuel.a.lopes@gmail.com>
2025-05-17 01:03:57 -0300

    ~ update scripts

--- cashback_v3.js
+++ cashback_v3.js
@@ -1,22 +1,23 @@
 (function () {
-    console.log($('#customer-cpf'))
-    console.log($('#customer-cpf').val())
-    console.log($('#customer-cpf').value)
-    console.log($('#customer-cpf').text)
-    // const target = document.querySelector('#customer-cpf');
+    const target = document.querySelector('#customer-cpf');
     // if (target) {
-    //     const observer = new MutationObserver((mutationsList) => {
-    //         mutationsList.forEach((mutation) => {
-    //             // if (mutation.type === 'attributes' && mutation.attributeName === 'value') {
-    //                 console.log('Valor alterado:', target.value);
-    //             // }
-    //         });
-    //     });
+        const observer = new MutationObserver((mutationsList) => {
+            mutationsList.forEach((mutation) => {
+                // if (mutation.type === 'attributes' && mutation.attributeName === 'value') {
+                console.log($('#customer-cpf'))
+                console.log($('#customer-cpf').val())
+                console.log($('#customer-cpf').value)
+                // }
+            });
+        });
     //
-    //     observer.observe(target, {
-    //         attributes: true,
-    //         // attributeFilter: ['value'] // só observa o atributo value
-    //     });
+        observer.observe(target, {
+            attributes: true,
+            childList: true,
+            subtree: true,
+            characterData: true
+            // attributeFilter: ['value'] // só observa o atributo value
+        });
     //
     //     console.log('Observando mudanças no atributo value de #customer-cpf');
     // } else {
