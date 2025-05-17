ce5dbe0468bcc40dc8ee6fe33c094db02b8c02a9
Samuel <samuel.a.lopes@gmail.com>
2025-05-17 01:06:11 -0300

    ~ update scripts

--- cashback_v3.js
+++ cashback_v3.js
@@ -4,6 +4,7 @@
         const observer = new MutationObserver((mutationsList) => {
             mutationsList.forEach((mutation) => {
                 // if (mutation.type === 'attributes' && mutation.attributeName === 'value') {
+                console.log('rodando por aqui')
                 console.log($('#customer-cpf'))
                 console.log($('#customer-cpf').val())
                 console.log($('#customer-cpf').value)
