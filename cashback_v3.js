78e2727b0ef6c474ec69cfc4bbd60281324acbcb
Samuel <samuel.a.lopes@gmail.com>
2025-05-17 01:12:03 -0300

    ~ update scripts

--- cashback_v3.js
+++ cashback_v3.js
@@ -1,27 +1,15 @@
 (function () {
-    const target = document.querySelector('#customer-cpf');
-    // if (target) {
-        const observer = new MutationObserver((mutationsList) => {
-            mutationsList.forEach((mutation) => {
-                // if (mutation.type === 'attributes' && mutation.attributeName === 'value') {
-                console.log('rodando por aqui')
-                console.log($('#customer-cpf'))
-                console.log($('#customer-cpf').val())
-                console.log($('#customer-cpf').value)
-                // }
-            });
-        });
-    //
-        observer.observe(target, {
-            attributes: true,
-            childList: true,
-            subtree: true,
-            characterData: true
-            // attributeFilter: ['value'] // só observa o atributo value
-        });
-    //
-    //     console.log('Observando mudanças no atributo value de #customer-cpf');
-    // } else {
-    //     console.warn('Elemento #customer-cpf não encontrado.');
-    // }
+    const el = document.querySelector('#customer-cpf');
+
+    let currentValue = el.value;
+
+    Object.defineProperty(el, 'value', {
+        get() {
+            return currentValue;
+        },
+        set(val) {
+            console.log('Interceptado! Novo valor:', val);
+            currentValue = val;
+        }
+    });
 })();
