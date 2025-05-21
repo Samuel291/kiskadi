3cf9f657ccc51a6a07f6b7b12a3140f1a5920bb5
Samuel <samuel.a.lopes@gmail.com>
2025-05-21 03:47:41 -0300

    ~ test scripts

--- cashback_v3.js
+++ cashback_v3.js
@@ -18,9 +18,9 @@ window.onload = function () {
     function kcheck(kdocument) {
         var amount = $('span[data-bind="money: checkout.subtotal"]').eq(0);
         var message = 'O cashback será resgatado como um cupom de desconto que será aplicado automaticamnete, não sendo cumulativo com outros cupons.';
-        // if (kdocument && amount.length) {
+        if (kdocument && amount.length) {
             if(kdocument.length >= 11) {
-                kreq('consulta', function (s) {
+                kreq(amount, 'consulta', function (s) {
                     $('<div class="ch-payment-group active selected" style="margin-top: 10px;" id="k-container">\n' +
                         '    <div class="ch-payment-group-header ch-flex" data-toggle="#k-content">\n' +
                         '        <div>\n' +
@@ -48,7 +48,7 @@ window.onload = function () {
                     $('#resgataCashback').click(function () {
                         $('#k-container').append('<style>@keyframes girar {' +
                             '0% { transform: rotate(0deg); }' +
-                            '100% { transform: rotate(360deg); }}');
+                            '100% { transform: rotate(360deg); }}</span>');
                         $('#k-container').append('<div id="k-preloader" style="border: 4px solid #f3f3f3; ' +
                             'border-top: 4px solid #333; ' +
                             'border-radius: 50%; ' +
@@ -56,7 +56,7 @@ window.onload = function () {
                             'height: 30px;' +
                             'animation: girar 1s linear infinite;' +
                             'margin: 20px auto;"></div>');
-                        kreq('resgate', function (y) {
+                        kreq(amount, 'resgate', function (y) {
                             $('input[name="coupon-identifier"]').val(y.coupon);
                             $('#coupon .link').click();
                             $('input[name="coupon-identifier"]')[0].dispatchEvent(new Event('input', {bubbles: true}));
@@ -70,11 +70,11 @@ window.onload = function () {
                 })
             }
             else{
-                consol.log('algo aqui')
+                console.log('algo aqui')
             }
-        // }
+        }
     }
-    function kreq(t, s, f) {
+    function kreq(amount, t, s, f) {
         if (typeof s !== 'function') {
             s = function() {};
         }
@@ -94,7 +94,6 @@ window.onload = function () {
             })
             .fail(function() {
                 f('Houve um erro ao tentar resgatar o cashback.');
-                console.log('deu erro')
             });
         console.log('......')
     }
